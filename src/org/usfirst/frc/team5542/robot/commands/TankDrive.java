184c2db40ebe18da6c75e9a3e4035a3a4dbd7315
weeryan17 <theweeryan17@gmail.com>
2017-02-08 17:27:24 -0600

    Tank drive changes
    Increased deadband.
    adjusted sensitivity.
    fixed sensitivity.

--- src/org/usfirst/frc/team5542/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team5542/robot/commands/TankDrive.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team5542.robot.commands;
 import org.usfirst.frc.team5542.robot.OI;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -24,29 +25,31 @@ public class TankDrive extends CommandBase {
     	XboxController xbox = oi.getXBox();
     	double left = -xbox.getRawAxis(OI.lyAxis);
     	double right = -xbox.getRawAxis(OI.ryAxis);
-    	if(left < 0.10 && left > -0.10){
+    	if(left < 0.20 && left > -0.20){
     		left = 0;
     	}
     	
-    	if(right < 0.10 && right > -0.10){
+    	if(right < 0.20 && right > -0.20){
     		right = 0;
     	}
     	
     	int pov = xbox.getPOV();
     	
-    	if(pov >= 315 || pov <= 45){
-    		sensitivity = sensitivity + .3;
+    	if(pov >= 315 || pov <= 45 && pov >= 0){
+    		sensitivity = sensitivity + .001;
     	} else if (pov <= 255 && pov >= 135){
-    		sensitivity = sensitivity - .3;
+    		sensitivity = sensitivity - .001;
     	}
     	
     	if(sensitivity <= 0){
     		sensitivity = 0;
     	}
     	
-    	if(sensitivity >= 2){
-    		sensitivity = 2;
+    	if(sensitivity >= 3){
+    		sensitivity = 3;
     	}
+    	
+    	SmartDashboard.putNumber("pov", pov);
     	
     	driveTrain.tankDrive(left, right, sensitivity);
     }
