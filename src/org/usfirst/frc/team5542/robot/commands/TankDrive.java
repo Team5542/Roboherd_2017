aaba8e18bfb851639cf4832c4ea8658a46c22ec1
weeryan17 <theweeryan17@gmail.com>
2017-02-08 17:01:34 -0600

    Added sensitivity.
    If it lags I will move it.

--- src/org/usfirst/frc/team5542/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team5542/robot/commands/TankDrive.java
@@ -15,15 +15,34 @@ public class TankDrive extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
     }
-
+    private static double sensitivity = 1.0;
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	OI oi = new OI();
     	XboxController xbox = oi.getXBox();
     	double left = -xbox.getRawAxis(OI.lyAxis);
     	double right = -xbox.getRawAxis(OI.ryAxis);
-    	driveTrain.tankDrive(left, right, 1);
+    	if(left < 0.10 && left > -0.10){
+    		left = 0;
+    	} 
+    	if(right < 0.10 && right > -0.10){
+    		right = 0;
+    	}
+    	int pov = xbox.getPOV();
+    	if(pov >= 315 || pov <= 45){
+    		sensitivity = sensitivity + .3;
+    	} else if (pov <= 255 && pov >= 135){
+    		sensitivity = sensitivity - .3;
+    	}
+    	if(sensitivity <= 0){
+    		sensitivity = 0;
+    	}
+    	if(sensitivity >= 2){
+    		sensitivity = 2;
+    	}
+    	driveTrain.tankDrive(left, right, sensitivity);
     }
 
     // Make this return true when this Command no longer needs to run execute()
