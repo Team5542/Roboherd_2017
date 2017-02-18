e1d6b4e2e8ea36e185e0f8a10620defef5bb05a9
weeryan17 <theweeryan17@gmail.com>
2017-02-18 09:04:09 -0600

    Gyro

--- src/org/usfirst/frc/team5542/robot/commands/CommandBase.java
+++ src/org/usfirst/frc/team5542/robot/commands/CommandBase.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team5542.robot.commands;
 import org.usfirst.frc.team5542.robot.subsystems.Lifter;
 import org.usfirst.frc.team5542.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team5542.robot.subsystems.Dropper;
+import org.usfirst.frc.team5542.robot.subsystems.GyroValues;
 import org.usfirst.frc.team5542.robot.subsystems.HoldBot;
 import org.usfirst.frc.team5542.robot.subsystems.PDPvalues;
 import org.usfirst.frc.team5542.robot.subsystems.Servos;
@@ -19,6 +20,7 @@ public abstract class CommandBase extends Command {
 	protected static HoldBot pid;
 	protected static Lifter arm;
 	protected static Dropper dropper;
+	protected static GyroValues gyro;
 	public static void init(){
 		driveTrain = DriveTrain.getInstance();
 		servos = Servos.getInstance();
@@ -26,5 +28,6 @@ public abstract class CommandBase extends Command {
 		pid = HoldBot.getInstance();
 		arm = Lifter.getInstance();
 		dropper = Dropper.getInstance();
+		gyro = GyroValues.getInstance();
 	}
 }
--- src/org/usfirst/frc/team5542/robot/subsystems/GyroValues.java
+++ src/org/usfirst/frc/team5542/robot/subsystems/GyroValues.java
@@ -1,14 +1,29 @@
 package org.usfirst.frc.team5542.robot.subsystems;
 
+import org.usfirst.frc.team5542.robot.commands.UpdateGyro;
+
+import com.analog.adis16448.frc.ADIS16448_IMU;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class GyroValues extends Subsystem {
 	
+	ADIS16448_IMU imu;
 	private GyroValues(){
-		
+		imu = new ADIS16448_IMU();
+	}
+	
+	public void updateGyro(){
+		double x = imu.getAngleX();
+		double y = imu.getAngleY();
+		double z = imu.getAngleZ();
+		SmartDashboard.putNumber("Gyro X", x);
+		SmartDashboard.putNumber("Gyro Y", y);
+		SmartDashboard.putNumber("Gyro Z", z);
 	}
 	
 	protected static GyroValues instance;
@@ -22,6 +37,7 @@ public class GyroValues extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new UpdateGyro());
     }
 }
 
